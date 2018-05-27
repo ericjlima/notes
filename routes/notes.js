b3a0adabc7a870868255a5332fc1c4a3481e9762
ericx2x <ericjlima@gmail.com>
2018-05-27 17:22:37 -0400

    bad backup of production

--- routes/notes.js
+++ routes/notes.js
@@ -15,33 +15,33 @@ con.connect(function(err) {
   if (err) throw err;
 });
 
- var sql = "CREATE TABLE notes (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), message VARCHAR(255), UNIQUE (name))";
-   con.query(sql, function (err, result) {
-     if (err) throw err;
-     console.log("Table created");
- });
+// var sql = "CREATE TABLE notes (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), message VARCHAR(255), UNIQUE (name))";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table created");
+// });
 
- var sql = "INSERT IGNORE INTO notes (name, message) VALUES ?";
- var values = [
- ['John', 'Highway 71'],
- ['Peter', 'Lowstreet 4'],
- ['Amy', 'Apple st 652'],
- ['Hannah', 'Mountain 21'],
- ['Michael', 'Valley 345'],
- ['Sandy', 'Ocean blvd 2'],
- ['Betty', 'Green Grass 1'],
- ['Richard', 'Sky st 331'],
- ['Susan', 'One way 98'],
- ['Vicky', 'Yellow Garden 2'],
- ['Ben', 'Park Lane 38'],
- ['William', 'Central st 954'],
- ['Chuck', 'Main Road 989'],
- ['Viola', 'Sideway 1633']
- ];
- con.query(sql, [values], function (err, result) {
- if (err) throw err;
- console.log("Number of records inserted: " + result.affectedRows);
- });
+// var sql = "INSERT IGNORE INTO notes (name, message) VALUES ?";
+// var values = [
+// ['John', 'Highway 71'],
+// ['Peter', 'Lowstreet 4'],
+// ['Amy', 'Apple st 652'],
+// ['Hannah', 'Mountain 21'],
+// ['Michael', 'Valley 345'],
+// ['Sandy', 'Ocean blvd 2'],
+// ['Betty', 'Green Grass 1'],
+// ['Richard', 'Sky st 331'],
+// ['Susan', 'One way 98'],
+// ['Vicky', 'Yellow Garden 2'],
+// ['Ben', 'Park Lane 38'],
+// ['William', 'Central st 954'],
+// ['Chuck', 'Main Road 989'],
+// ['Viola', 'Sideway 1633']
+// ];
+// con.query(sql, [values], function (err, result) {
+// if (err) throw err;
+// console.log("Number of records inserted: " + result.affectedRows);
+// });
 
 
 
