f0964063d6d73d088c2a8b6be2dccd5b94369e0b
ericx2x <ericjlima@gmail.com>
2019-12-02 19:26:45 -0500

    placed cors where it should be?

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ var subnotesRouter = require('./routes/subnotes');
 var passwordRouter = require('./routes/password');
 var bodyParser = require('body-parser');
 
+var cors = require('cors');
+
 var app = express();
 
+app.use(cors({origin: ["http://ericnote.us", "http://www.ericnote.us"], credentials: true}));
 
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); 
 //var cors = require('cors');
 var app = express();
 var mysql = require('mysql');
