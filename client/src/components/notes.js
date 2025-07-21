0ecf4aa0a3e2d2587669b45d447fe9cfc812a406
ericx2x <ericjlima@gmail.com>
2025-07-21 16:57:36 +0800

    fix to make my notes usable after failing to think about subnotes on exisitng production notes

--- client/src/components/notes.js
+++ client/src/components/notes.js
@@ -550,7 +550,10 @@ const Notes = props => {
           <div className="noteWriteContent">
             {/*TODO: put this in a better div*/}
             <h1>{toTitleCase(props.match.params.id)}</h1>
-            {isLoggedIn && userOnOwnPath() && (
+          /*TODO: think about subnotes on production.. this temp fix gets things renabled for yourself
+          && userOnOwnPath()
+          */
+            {isLoggedIn && (
               <div>
                 <div className="topRow">
                   <p className="verificationMessage">{verificationMessage} </p>
